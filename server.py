7b59fb5d72eaa369fd68b31f0aef7fb846e736f7
Nicholas Harvey <nicholasharvey10@gmail.com>
2016-07-26 10:15:42 -0700

    day two push

--- server.py
+++ server.py
@@ -2,10 +2,18 @@ from flask import Flask, session, render_template, request, redirect
 import random
 
 app=Flask(__name__)
+app.secret_key = 'secret'
 
 @app.route('/')
 def index():
+    try:
+        session['random']
+    except:
+        session['random'] = random.randrange(1,101)
+    print session['random']
     return render_template('index.html')
 
+@app.route('/check', methods=['POST'])
 
-app.run(debug=True) 
+
+app.run(debug=True)
